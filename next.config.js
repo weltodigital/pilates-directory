95eabcee86e6a13c1c01635ddb50749d80c09c1b
Ed Welton <edwelton@Eds-MacBook-Air.local>
2025-10-08 08:32:55 +0100

    Add Google image domains to Next.js image config

--- next.config.js
+++ next.config.js
@@ -13,6 +13,14 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'maps.googleapis.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh5.googleusercontent.com',
+      },
     ],
   },
 }
